9a21f2a8b337be4b1eb1b6f090c7853ef4ade651
Kyle Hu <lloydpearce4@gmail.com>
2025-12-22 11:47:54 +0800

    first commit

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,23 @@
+import { Target } from "lucide-react"
+import { Button } from "@/src/components/ui/button"
+
+const NavBar = () => {
+	return (
+		<header className="border-b-2">
+			<nav className="flex justify-between items-center mx-auto px-4 py-6 container">
+				<div className="flex items-center space-x-2">
+					<Target className="w-8 h-8 text-blue-600" />
+					<span className="font-bold text-gray-900 dark:text-gray-100 text-2xl">
+						AppTrackr
+					</span>
+				</div>
+				<div className="flex items-center space-x-4">
+					<Button variant="ghost">Sign In</Button>
+					<Button>Get Started</Button>
+				</div>
+			</nav>
+		</header>
+	)
+}
+
+export default NavBar
